c7df008499f3df42e1201d9b3a7869764fa7155f
Fletcher Nichol <fnichol@nichol.ca>
2011-04-10 16:29:15 -0600

    Update debian postinstall script.

--- common/debian/postinstall.sh
+++ common/debian/postinstall.sh
@@ -1,39 +1,92 @@
-#http://adrianbravo.tumblr.com/post/644860401
+# **postinstall.sh** is a script executed after Ubuntu has been installed and
+# restarted. There is no user interaction so all commands must be able to run
+# in a non-interactive mode.
+#
+# If any package install time questions need to be set, you can use
+# `preeseed.cfg` to populate the settings.
 
-# Update the box
+
+### Additional Ubuntu Package Repositories
+
+# There is an Opscode *apt* repository with current builds of chef that
+# only depend on the package installed Ruby. At present, I cannot find
+# a complimentary repository for updated puppet packages and as a result
+# an older "safe" package will be installed from the main Ubuntu *apt*
+# repository.
+
+# Add the Opscode *apt* repository by creating a `.list` file and adding
+# the repository key
+echo "deb http://apt.opscode.com/ `lsb_release -cs` main" \
+  >/etc/apt/sources.list.d/opscode.list
+wget -q -O- http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
+
+
+# Now update the system with `update` and `upgrade`. The `update` will
+# also fetch the package manifest from any additionally added repositories.
+# The `upgrade` will update any out of date packages but will almost certainly
+# do nothing as we've installed from the netboot installer and all packages
+# should be up to date.
 apt-get -y update
-apt-get -y install linux-headers-$(uname -r) build-essential 
-apt-get -y install zlib1g-dev libssl-dev libreadline5-dev
-apt-get -y install curl unzip
-apt-get clean
+apt-get -y upgrade
 
-# Set up sudo
-cp /etc/sudoers /etc/sudoers.orig
+### Customize Sudoers
+
+# The main user (`vagrant` in our case) needs to have **password-less** sudo
+# access as described in the Vagrant base box
+# [documentation](http://vagrantup.com/docs/base_boxes.html#setup_permissions).
+# This user belongs to the `admin` group, so we'll change that line.
 sed -i -e 's/%sudo ALL=(ALL) ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers
 
-# Install Ruby from packages
-apt-get -y install ruby ruby-dev libopenssl-ruby1.8 irb ri rdoc
+### Chef and Puppet Packages
 
-# Install Rubygems from source
-rg_ver=1.6.2
-curl -o /tmp/rubygems-${rg_ver}.zip \
-  "http://production.cf.rubygems.org/rubygems/rubygems-${rg_ver}.zip"
-(cd /tmp && unzip rubygems-${rg_ver}.zip && \
-  cd rubygems-${rg_ver} && ruby setup.rb --no-format-executable)
-rm -rf /tmp/rubygems-${rg_ver} /tmp/rubygems-${rg_ver}.zip
+# The Vagrant base box
+# [documentation](http://vagrantup.com/docs/base_boxes.html#boot_and_setup_basic_software)
+# specifies that both the Puppet and Chef gems should be installed to qualify
+# as a complete Vagrant base box.
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y install chef
+apt-get -y install puppet
 
-# Install Chef & Puppet
-gem install chef --no-ri --no-rdoc
-gem install puppet --no-ri --no-rdoc
+### Vagrant SSH Keys
 
-# Install vagrant keys
+# Since Vagrant only supports key-based authentication for SSH, we must
+# set up the vagrant user to use key-based authentication. We can get the
+# public key used by the Vagrant gem directly from its Github repository.
 mkdir -p /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
-cd /home/vagrant/.ssh
-curl -o /home/vagrant/.ssh/authorized_keys \
-  'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub'
+(cd /home/vagrant/.ssh &&
+  wget --no-check-certificate \
+    'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
+    -O /home/vagrant/.ssh/authorized_keys)
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
+
+### VirtualBox Guest Additions
+
+# The netboot installs the VirtualBox support (old) so we have to remove it
+apt-get -y remove virtualbox-ose-guest-dkms
+apt-get -y remove virtualbox-ose-guest-utils
+
+# The Guest Additions installer will require the use of a compiler, so we'll
+# install it for the moment
+apt-get -y install linux-headers-$(uname -r) build-essential
+
+# Now download the current VirtualBox guest additions from the VirtualBox
+# website, mount, and install it
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+(cd /tmp &&
+  wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso)
+mount -o loop /tmp/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+yes|sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+
+# Cleanup the downloaded ISO
+rm -f /tmp/VBoxGuestAdditions_$VBOX_VERSION.iso
+
+# Remove the build tools again to keep things pristine
+apt-get -y remove linux-headers-$(uname -r) build-essential
+
+### Misc. Tweaks
 
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config
@@ -41,21 +94,11 @@ echo 'UseDNS no' >> /etc/ssh/sshd_config
 # Customize the message of the day
 echo 'Welcome to your Vagrant-built virtual machine.' > /var/run/motd
 
-# The netboot installs the VirtualBox support (old) so we have to remove it
-apt-get -y remove virtualbox-ose-guest-dkms
-apt-get -y remove virtualbox-ose-guest-utils
+### Clean up
 
-# Install the VirtualBox guest additions
-VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-curl -Lo /tmp/VBoxGuestAdditions_$VBOX_VERSION.iso \
-  "http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso"
-mount -o loop /tmp/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
-yes|sh /mnt/VBoxLinuxAdditions.run
-umount /mnt
-
-# Clean up
-apt-get -y remove linux-headers-$(uname -r) build-essential
 apt-get -y autoremove
+apt-get -y clean
 
-rm /tmp/VBoxGuestAdditions_$VBOX_VERSION.iso 
 exit
+
+# And we're done.
