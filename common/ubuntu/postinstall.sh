624662e8f44afa63ced769f2c7b45d1298158ded
Fletcher Nichol <fnichol@nichol.ca>
2011-07-02 13:59:29 -0600

    Add additional cleanup from veewee project definitions.

--- common/ubuntu/postinstall.sh
+++ common/ubuntu/postinstall.sh
@@ -143,6 +143,18 @@ apt-get -y remove build-essential
 apt-get -y autoremove
 apt-get -y clean
 
+# Removing leftover leases and persistent rules
+rm /var/lib/dhcp3/*
+
+# Make sure Udev doesn't block our network, see: http://6.ptmc.org/?p=164
+rm /etc/udev/rules.d/70-persistent-net.rules
+mkdir /etc/udev/rules.d/70-persistent-net.rules
+rm -rf /dev/.udev/
+rm /lib/udev/rules.d/75-persistent-net-generator.rules
+
+# Add a 2 sec delay to the interface up, to make the dhclient happy
+echo "pre-up sleep 2" >> /etc/network/interfaces
+
 ### Compress Image Size
 
 # Zero out the free space to save space in the final image
