213f467adbc2672cb8ab6344e2531dc5cc7f7766
Fletcher Nichol <fnichol@nichol.ca>
2011-07-19 16:27:32 -0600

    Port ubuntu-11.04 boot params and preseed tweaks to ubuntu-10.10.

--- common/ubuntu-10.10/config.rb
+++ common/ubuntu-10.10/config.rb
@@ -1,3 +1,9 @@
+#
+# For reference, please see:
+# * https://help.ubuntu.com/10.10/installation-guide/i386/boot-parms.html
+# * https://help.ubuntu.com/10.10/installation-guide/i386/preseed-contents.html
+# * https://help.ubuntu.com/10.10/installation-guide/i386/ch05s01.html
+# * https://help.ubuntu.com/8.04/installation-guide/i386/preseed-using.html
 module VeeWee
   module Common
     module Ubuntu1010
@@ -6,12 +12,25 @@ module VeeWee
           :kickstart_port       => "7123",
           :ssh_host_port        => "7223",
           :boot_cmd_sequence    => [ 
-            '<Tab>',
-            'noapic preseed/url=http://%IP%:%PORT%/preseed.cfg ',
-            'debian-installer=en_US auto locale=en_US kbd-chooser/method=us ',
-            'hostname=%NAME% ',
-            'fb=false debconf/frontend=noninteractive ',
-            'console-setup/ask_detect=false console-setup/modelcode=pc105 console-setup/layoutcode=us ',
+            '<Esc>',
+            'install ',
+            'noapic ',
+            'auto-install/enable ',
+            'console-setup/ask_detect=false ',
+            'console-setup/layoutcode=us ',
+            'console-setup/modelcode=pc105 ',
+            'debconf/frontend=noninteractive ',
+            'debconf/priority=critical ',
+            'debian-installer/locale=en_US ',
+            'debian-installer/framebuffer=false ',
+            'kbd-chooser/method=us ',
+            'netcfg/choose_interface=auto ',
+            'netcfg/dhcp_timeout=60 ',
+            'netcfg/get_hostname=%NAME% ',
+            'preseed/interactive=false ',
+            'preseed/url=http://%IP%:%PORT%/preseed.cfg ',
+            'vga=normal ',
+            'DEBCONF_DEBUG=5 ',
             ' -- <Enter>' 
           ],
           :kickstart_file       => "preseed.cfg",
