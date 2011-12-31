4c61a725b557005ea0abacb0530cbd83af569cc2
Fletcher Nichol <fnichol@nichol.ca>
2011-12-31 10:52:22 -0700

    Update os_type_id on oneiric64.

--- definitions/ubuntu-11.10-server-amd64-netboot/definition.rb
+++ definitions/ubuntu-11.10-server-amd64-netboot/definition.rb
@@ -4,7 +4,7 @@ end
 
 Veewee::Session.declare(
   VeeWee::Common.config.merge(VeeWee::Common::Ubuntu1104.config).merge(
-    :os_type_id           => 'Ubuntu',
+    :os_type_id           => 'Ubuntu_64',
     :iso_file             => "ubuntu-11.10-server-amd64-netboot.iso",
     :iso_src              => "http://archive.ubuntu.com/ubuntu/dists/oneiric/main/installer-amd64/current/images/netboot/mini.iso",
     :iso_md5              => "407bf3a2b1d56af8e01aecfc6fdcbc9d"
